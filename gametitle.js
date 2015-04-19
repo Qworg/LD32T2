88e055ad10cd5df96e9ef1f498ab383a623a3bd1
Ed Paradis <legomaniac@gmail.com>
2015-04-19 16:38:22 -0700

    add solarized palette; prevent autoscaling of canvas; improve game titlescreen;

--- gametitle.js
+++ gametitle.js
@@ -1,14 +1,31 @@
-var gameTitle = function(game){}
+var gameTitle = function(game){};
 
 gameTitle.prototype = {
-    create: function(){
-    var style = { font: "32px Arial", fill: "#ff0044", align: "center" };
-    var gameTitle = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Wizard School Simulator", style);
-    gameTitle.anchor.setTo(0.5,0.5);
-    var playButton = this.game.add.button(160,320,"play",this.playTheGame,this);
-    playButton.anchor.setTo(0.5,0.5);
-  },
-  playTheGame: function(){
-    this.game.state.start("TheGame");
-  }
-}
+    create: function() {
+      var centerX = this.game.world.centerX;
+
+      this.game.stage.backgroundColor = solarized.base3;
+
+      var titleStyle = { font: "bold 32px Arial", fill: solarized.red, align: "center" };
+      var gameTitle = this.game.add.text(centerX, this.game.world.height * 0.2,
+          "Wizard School Simulator", titleStyle);
+      gameTitle.anchor.setTo(0.5,0.5);
+
+      var instructionsStyle = { font: "bold 14px Arial", fill: solarized.base01, align: "center",
+          wordWrap: true, wordWrapWidth: this.game.world.width * 0.7 };
+      var instructions = this.game.add.text(centerX, this.game.world.height * 0.5,
+          "Instructions: The Warthog School for Magical Children needs a new " +
+          "headmaster! You must sort the incoming students and get them through " +
+          "the school year. Can you get students all the way to 7th year and graduation?",
+          instructionsStyle );
+      instructions.anchor.setTo(0.5,0.5);
+
+      var playButton = this.game.add.button(centerX, this.game.world.height * 0.8,
+          "play", this.playTheGame, this);
+      playButton.anchor.setTo(0.5,0.5);
+    },
+
+    playTheGame: function(){
+        this.game.state.start("TheGame");
+    }
+};
