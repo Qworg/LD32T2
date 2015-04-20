4ce8728da89928d62e22c8170b2d6a8dd6364104
Ed Paradis <legomaniac@gmail.com>
2015-04-19 19:57:25 -0700

    start of sorting screen

--- thegame.js
+++ thegame.js
@@ -5,11 +5,35 @@ var theGame = function(game) {
 theGame.prototype = {
   create: function() {
     score = Math.floor(Math.random()*100);
-    overButton = this.game.add.button(100,100,"lower", this.clickedOver, this);
-    overButton.anchor.setTo(0.5,0.5);
+    var width = this.game.world.width;
+    
+    for( var i = 0; i < 4; i += 1) {
+      this.makeHouseDisplay( (width*i/4) + width/8 , this.game.world.height * 0.5, "house"+i);
+    }
+
+    this.makeInstructions();
   },
   clickedOver: function() {
     console.log('clicked game over button');
     this.game.state.start("GameOver",true, false, score);
+  },
+  makeHouseDisplay : function( x, y, houseName) {
+    var selectButton = this.game.add.button(x, y, 'smBlankBtn', this.houseSelected, this);
+    selectButton.anchor.setTo(0.5,0.5);
+    selectButton.houseName = houseName;
+    var titleStyle = { font: "bold 14px Arial", fill: solarized.base3, align: 'center' };
+    var nameTitle = this.game.add.text(x,y, houseName, titleStyle );
+    nameTitle.anchor.setTo(0.5,0.5);
+  },
+  houseSelected : function(button) {
+    console.log('you selected House ' + button.houseName);
+  },
+  makeInstructions : function() {
+    var instructionsStyle = { font: "bold 14px Arial", fill: solarized.base01, align: "center",
+      wordWrap: true, wordWrapWidth: this.game.world.width * 0.7 };
+    var instructions = this.game.add.text(this.game.world.width/2, this.game.world.height*0.4,
+      "Sort the incoming students!",
+      instructionsStyle );
+    instructions.anchor.setTo(0.5, 0.5);
   }
 }
